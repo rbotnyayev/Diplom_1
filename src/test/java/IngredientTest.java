8a0688c942b3df529294b933c276011a93318d02
Ruslan Botnyayev <rbotnyayev@yandex.ru>
2024-03-10 00:35:00 +0500

    Done: tests and Jacoco

---
+++ src/test/java/IngredientTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class IngredientTest {
+    public IngredientType type;
+    public String name;
+    public float price;
+
+    public IngredientTest(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][]{
+                {IngredientType.SAUCE, "Томатный соус", 50.0f},
+                {IngredientType.FILLING, "Сыр моцарелла", 70.0f}
+        };
+    }
+
+    @Test
+    public void testIngredient() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+
+        assertEquals(type, ingredient.getType());
+        assertEquals(name, ingredient.getName());
+        assertEquals(price, ingredient.getPrice(), 0.01);
+    }
+}
